a1309865b55d0914be3a6985c52440b76af3172c
Yasuo Yuasa <y_yuasa@tec.tomakomai.or.jp>
2017-10-19 07:12:26 +0000

    scaffold

---
+++ app/views/taggings/_tagging.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! tagging, :id, :book_id, :tag_id, :created_at, :updated_at
+json.url tagging_url(tagging, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019042246) do
+ActiveRecord::Schema.define(version: 20171019070852) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
