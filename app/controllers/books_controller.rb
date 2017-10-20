1a238eb5197d8ff6530e4f2d4052e078cfdd6b17
Yasuo Yuasa <y_yuasa@tec.tomakomai.or.jp>
2017-10-20 05:43:08 +0000

    booksとtagsの登録メッセージスタイルを変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -71,6 +71,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :published_on, :showing, :price, :tag_ids [])
+      params.require(:book).permit(:title, :author, :published_on, :showing, :price)
     end
 end
