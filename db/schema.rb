fb0db7880cf2ab2c9a5a5e0bcc225743ddf16a12
Yasuo Yuasa <y_yuasa@tec.tomakomai.or.jp>
2017-10-25 04:55:02 +0000

    orderモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020073848) do
+ActiveRecord::Schema.define(version: 20171025044726) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20171020073848) do
     t.string "picture_content_type"
     t.integer "picture_file_size"
     t.datetime "picture_updated_at"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_orders_on_book_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "taggings", force: :cascade do |t|
