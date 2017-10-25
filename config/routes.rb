ed666053533937c5425f63cd05ad64fd5be264c0
Yasuo Yuasa <y_yuasa@tec.tomakomai.or.jp>
2017-10-25 04:30:22 +0000

    商品画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-  get 'products/show'
 
   devise_for :users
   resources :taggings
   resources :tags, expect: :show
   resources :books
+  resources :products, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
